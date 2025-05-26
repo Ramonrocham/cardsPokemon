8138937be175611da41042c3478622a98605d801
ramonrocham <anacletorrocham@gmail.com>
2025-05-26 13:05:22 -0300

    TiposPokemons.php adding

---
+++ tiposPokemon.php
@@ -0,0 +1,22 @@
+<?php
+
+$tiposPokemon = [
+'Normal' => ['ptbr'=> 'fogo', 'cor'=> '#A8A77A'],
+'Fire' => ['ptbr'=> 'Fogo', 'cor'=> '#EE8130'],
+'Water' => ['ptbr'=> 'Água', 'cor'=> '#6390F0'],
+'Grass' => ['ptbr'=> 'Grama', 'cor'=> '#7AC74C'],
+'Electric' => ['ptbr'=> 'Eletrico', 'cor'=> '#F7D02C'],
+'Ice' => ['ptbr'=> 'Gelo', 'cor'=> '#96D9D6'],
+'Fighting' => ['ptbr'=> 'Lutador', 'cor'=> '#C22E28'],
+'Poison' => ['ptbr'=> 'Venenoso', 'cor'=> '#A33EA1'],
+'Ground' => ['ptbr'=> 'Terra', 'cor'=> '#E2BF65'],
+'Flying' => ['ptbr'=> 'Voador', 'cor'=> '#A98FF3'],
+'Psychic' => ['ptbr'=> 'Psíquico', 'cor'=> '#F95587'],
+'Bug' => ['ptbr'=> 'Inseto', 'cor'=> '#A6B91A'],
+'Rock' => ['ptbr'=> 'Pedra', 'cor'=> '#B6A136'],
+'Ghost' => ['ptbr'=> 'Fantasma', 'cor'=> '#735797'],
+'Dragon' => ['ptbr'=> 'Dragão', 'cor'=> '#6F35FC'],
+'Dark' => ['ptbr'=> 'Sombrio', 'cor'=> '#705746'],
+'Steel' => ['ptbr' => 'Metal', 'cor' => '#B7B7CE'],
+'Fairy' => ['ptbr' => 'Fada', 'cor' => '#D685Ad'],
+];
